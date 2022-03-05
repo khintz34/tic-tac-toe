6a91e794f8d9a39e622c71c4a22cd8674def466c
David Dunkovich <31701641+DavidDunkovich@users.noreply.github.com>
2022-03-04 16:30:04 -0800

    study sesh xoxo

--- script.js
+++ script.js
@@ -1,83 +1,76 @@
-let player1 = 'Player 1';
-let player2 = 'Player 2';
 let whosTurn = 1;
 let countOfPlays = 0;
-let gameArray = ['1','2','3','4','5','6','7','8','9'];
-
-// query selectors
-const btn1 = document.querySelector('.btn1');
-const btn2 = document.querySelector('.btn2');
-const btn3 = document.querySelector('.btn3');
-const btn4 = document.querySelector('.btn4');
-const btn5 = document.querySelector('.btn5');
-const btn6 = document.querySelector('.btn6');
-const btn7 = document.querySelector('.btn7');
-const btn8 = document.querySelector('.btn8');
-const btn9 = document.querySelector('.btn9');
-const gameBtn = document.querySelectorAll('.gameBtn');
-const showWinner = document.querySelector('.winner');
+let gameArray = [];
 
 window.onload = turnOnEventListeners;
 
+function onGameBoardClick(e) {
+  if (e.target.innerHTML !== "") {
+    return;
+  }
+
+  const index = e.target.getAttribute("data-value");
+
+  countOfPlays++;
+
+  if (whosTurn === 1) {
+    e.target.innerHTML = "X";
+    whosTurn = 2;
+    gameArray[index] = "X";
+  } else {
+    e.target.innerHTML = "O";
+    whosTurn = 1;
+    gameArray[index] = "O";
+  }
+
+  if (countOfPlays >= 0) {
+    checkForWinner();
+  }
+  console.log(gameArray);
+}
 
 function turnOnEventListeners() {
-    btn1.onclick = playGame;
-    btn2.onclick = playGame;
-    btn3.onclick = playGame;
-    btn4.onclick = playGame;
-    btn5.onclick = playGame;
-    btn6.onclick = playGame;
-    btn7.onclick = playGame;
-    btn8.onclick = playGame;
-    btn9.onclick = playGame;
+  const gameboard = document.querySelector(".gameboard");
+  gameboard.onclick = onGameBoardClick;
 }
 
-
-
-function playGame() {
-    if (whosTurn === 1) {
-        this.innerHTML = 'X';
-        whosTurn = 2;
-        countOfPlays++;
-        this.onclick = '';
-        gameArray[this.value] = 'X';
-    } else {
-        this.innerHTML = 'O';
-        whosTurn = 1;
-        countOfPlays++;
-        this.onclick = '';
-        gameArray[this.value] = 'O';
-    }
-
-    if (countOfPlays >= 0) {
-        checkForWinner();
-    }
-console.log(gameArray)
-}
-
+const combinations = [
+  [0, 1, 2],
+  [0, 3, 6],
+];
 
 function checkForWinner() {
-    if (gameArray[0] === gameArray[1] && gameArray[0] === gameArray[2]
-        || gameArray[0] === gameArray[3] && gameArray[0] === gameArray[6]
-        || gameArray[0] === gameArray[4] && gameArray[0] === gameArray[8]
-        || gameArray[3] === gameArray[4] && gameArray[3] === gameArray[5]
-        || gameArray[1] === gameArray[4] && gameArray[1] === gameArray[7]
-        || gameArray[2] === gameArray[4] && gameArray[2] === gameArray[6]
-        || gameArray[6] === gameArray[7] && gameArray[6] === gameArray[8]
-        || gameArray[2] === gameArray[5] && gameArray[2] === gameArray[8])
-    { 
-        console.log('Winner');
-        btn1.onclick = '';
-        btn2.onclick = '';
-        btn3.onclick = '';
-        btn4.onclick = '';
-        btn5.onclick = '';
-        btn6.onclick = '';
-        btn7.onclick = '';
-        btn8.onclick = '';
-        btn9.onclick = '';
+  const winningIndex = combinations.findIndex((c) => isWin(c[0], c[1], c[2]));
 
-        showWinner.style.display='block';
-    }
+  if (!winningIndex) {
+    return;
+  }
 
+  // Use the winning index to do whatever
+
+  //   if (
+  //     isWin(0, 1, 2) ||
+  //     isWin(0, 3, 6)
+  //     // (gameArray[0] === gameArray[4] && gameArray[0] === gameArray[8]) ||
+  //     // (gameArray[3] === gameArray[4] && gameArray[3] === gameArray[5]) ||
+  //     // (gameArray[1] === gameArray[4] && gameArray[1] === gameArray[7]) ||
+  //     // (gameArray[2] === gameArray[4] && gameArray[2] === gameArray[6]) ||
+  //     // (gameArray[6] === gameArray[7] && gameArray[6] === gameArray[8]) ||
+  //     // (gameArray[2] === gameArray[5] && gameArray[2] === gameArray[8])
+  //   ) {
+  //     console.log("Winner");
+  //     const showWinner = document.querySelector(".winner");
+
+  //     showWinner.style.display = "block";
+  //   }
+}
+
+function isWin(index1, index2, index3) {
+  // if (any are blank) {
+  //     return false;
+  // }
+  return (
+    gameArray[index1] === gameArray[index2] &&
+    gameArray[index1] === gameArray[index3]
+  );
 }
